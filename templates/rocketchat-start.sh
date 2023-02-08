5e18b2886bb4fb23a294ba7108d950d0bcffe084
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2023-02-08 22:56:34 +0100

    fixes for rocketchat start script
    use public root url
    define mail_url for smtp

--- templates/rocketchat-start.sh
+++ templates/rocketchat-start.sh
@@ -5,10 +5,11 @@ export PID=$HOME/var/run/rocketchat.pid
 source $HOME/.profile
 cd $HOME/Rocket.Chat
 
-export ROOT_URL=http://localhost:{{rocketchat_port}}
+export ROOT_URL=https://{{domain}}/
 export PORT={{rocketchat_port}}
-export MONGO_URL="mongodb://localhost:{{mongod_port}}/rocketchat?replicaSet=rs01"
-export MONGO_OPLOG_URL="mongodb://localhost:{{mongod_port}}/local?replicaSet=rs01"
+export MONGO_URL="mongodb://127.0.0.1:{{mongod_port}}/rocketchat?replicaSet=rs01"
+export MONGO_OPLOG_URL="mongodb://127.0.0.1:{{mongod_port}}/local?replicaSet=rs01"
+export MAIL_URL="smtp://{{pac}}-{{user}}:{{password}}@{{pac}}.hostsharing.net:587"
 
-nohup node main.js > $HOME/var/log/rocketchat.log 2>&1 &
+nohup node main.js >> $HOME/var/log/rocketchat.log 2>&1 &
 echo $! > $PID
